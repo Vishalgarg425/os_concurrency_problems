b3a16a3d5e7df50c46533eef38c785a2550b365d
Vishal Garg <32809293+Vishalgarg425@users.noreply.github.com>
2019-04-01 18:07:09 +0530

    Update reader_writ_sol.c

--- reader_writ_sol.c
+++ reader_writ_sol.c
@@ -1,6 +1,86 @@
 #include<stdio.h>
 #include<unistd.h>
-#include "semaphoremine.h" /* sem_t */
+// #include "semaphoremine.h" /* sem_t */
+#include <stdio.h>
+// #include <threads.h>
+#include<pthread.h>
+#include<stdlib.h> 
+#include <stdatomic.h>
+
+atomic_int atomic;
+typedef struct 
+{
+        atomic_int semaph;
+        pthread_mutex_t wait;
+} sem_t;
+
+void  sem_init(sem_t* sem, unsigned int value){
+        sem->semaph=value;
+}
+
+void proberen(sem_t *sem){
+    pthread_mutex_lock(&sem->wait); 
+    while (1)  {
+          if (sem->semaph <= 0) 
+          {
+             pthread_mutex_unlock(&sem->wait);
+             continue;
+          } 
+          else 
+          {
+              pthread_mutex_unlock(&sem->wait);
+              sem->semaph = sem->semaph - 1;
+             ;break;
+          }
+       }    
+}
+/* verhogen - to increment */
+void verhogen(sem_t *sem)
+{ 
+    // pthread_mutex_lock(&sem->lock1);  
+    sem->semaph = sem->semaph + 1;
+    // pthread_mutex_unlock(&sem->lock1);
+}
+#define NO_OF_SEATS 13//set whatever no u want keep it as large as possible
+#define NO_CSTR_THREADS 4
+
+sem_t barbmutex;
+sem_t seatmutex;
+sem_t cstmrmutex;
+int no_of_free_seats=13;
+// 
+void* barber(void* num) 
+{ 
+    while(1){
+        proberen(&cstmrmutex);
+  		proberen(&seatmutex);
+        sleep(rand()%3);
+        no_of_free_seats+=1; 
+        printf("done cutting\n"); 
+        printf("%d seats left\n\n",no_of_free_seats); 
+        fflush(stdout);
+        verhogen(&barbmutex);
+        verhogen(&seatmutex);  
+    } 
+}
+void* customer(void* num) 
+{ 
+    while(1){
+  		proberen(&seatmutex);
+          if(no_of_free_seats>0){
+              no_of_free_seats--;
+              printf("new customer arrived\n");
+              printf("%d seats left\n\n",no_of_free_seats);
+              fflush(stdout);
+              verhogen(&cstmrmutex);
+              verhogen(&seatmutex);
+              proberen(&barbmutex);
+          }
+          else{
+              verhogen(&seatmutex);
+          }
+    } 
+}
 #define BUFFER_SIZE 10
 #define NO_WRIT_THREADS 13
 #define NO_READ_THREADS 11
