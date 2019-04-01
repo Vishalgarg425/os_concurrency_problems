459d487ce2f6210a76e1a57fe624ea628ec502bc
Vishal Garg <32809293+Vishalgarg425@users.noreply.github.com>
2019-04-01 18:05:09 +0530

    Update cons_prod_sol.c

--- cons_prod_sol.c
+++ cons_prod_sol.c
@@ -1,6 +1,47 @@
 #include<stdio.h>
 #include<unistd.h>
-#include "semaphoremine.h" /* sem_t */
+// #include "semaphoremine.h" sem_t
+#include <stdio.h>
+// #include <threads.h>
+#include<pthread.h>
+#include<stdlib.h> 
+#include <stdatomic.h>
+
+atomic_int atomic;
+typedef struct 
+{
+        atomic_int semaph;
+        pthread_mutex_t wait;
+} sem_t;
+
+void  sem_init(sem_t* sem, unsigned int value){
+        sem->semaph=value;
+}
+
+void proberen(sem_t *sem){
+    pthread_mutex_lock(&sem->wait); 
+    while (1)  {
+          if (sem->semaph <= 0) 
+          {
+             pthread_mutex_unlock(&sem->wait);
+             continue;
+          } 
+          else 
+          {
+              pthread_mutex_unlock(&sem->wait);
+              sem->semaph = sem->semaph - 1;
+             ;break;
+          }
+       }    
+}
+/* verhogen - to increment */
+void verhogen(sem_t *sem)
+{ 
+    // pthread_mutex_lock(&sem->lock1);  
+    sem->semaph = sem->semaph + 1;
+    // pthread_mutex_unlock(&sem->lock1);
+}
+
 #define BUFFER_SIZE 10
 #define NO_PROD_THREADS 13
 #define NO_CONS_THREADS 8
